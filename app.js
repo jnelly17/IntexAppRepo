ffe4c0a307cc27bce63d2c28ef635c053c2b2cea
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-04 18:22:02 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-let express = require("express");
+const express = require("express")
+
 let app = express();
 
-app.get("/", function(req,res) {
-    res.send("IT WORKS")
-} )
+let path = require("path")
 
-app.listen(process.env.PORT || 5000);
-module.exports = app;
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs")
+
+app.use(express.urlencoded({ extended:true}))
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || "localhost",
+        user: process.env.RDS_USERNAME || "postgres",
+        password: process.env.RDS_PASSWORD || "password",
+        database: process.env.RDS_DB_NAME || "IntexFall",
+        port: process.env.RDS_PORT || 5432,
+        ssl:process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+})
+
+app.get("/", (req, res) => {
+    knex.select().from("country").then( country => {
+        res.render("displayCountry", { mycountry : country });
+    })
+})
+
+app.listen(port, () => console.log("I am listening"))
