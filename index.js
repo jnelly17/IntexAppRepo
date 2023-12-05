454e32dfc31d7fa42918a0c6ac1bb99312f7abdd
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-04 17:13:23 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let app = express();
-
-app.get("/", function(req,res) {
-    res.send("IT WORKS")
-} )
-
-app.listen(process.env.PORT || 5000);
-module.exports = app;
