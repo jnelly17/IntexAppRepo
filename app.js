50669d5f3caa44f39ea3c6abccea0ba365d615b7
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-05 14:26:11 -0700

    version 3 main

--- app.js
+++ app.js
@@ -1,31 +1,34 @@
-const express = require("express")
+var express = require("express");
+var path = require("path");
 
-let app = express();
+var app = express();
+const router = express.Router(); // Moved router definition to the top
 
-let path = require("path")
+app.set("port", process.env.PORT || 3000);
 
-const port = process.env.PORT || 3000;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs"); // Corrected the setting for view engine
 
-app.set("view engine", "ejs")
+app.use("/", router); // Use the router for the root path
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.urlencoded({ extended:true}))
+router.get("/", function(req, res){
+    console.log("start page active");
+    res.render("index"); // Removed .ejs since the view engine is set
+});
+router.get("/survey", function(req, res) {
+    console.log("survey page active");
+    res.render("survey");
+});
+router.get("/awareness", function(req, res) {
+    console.log("awareness page active");
+    res.render("awareness");
+});
+router.get("/login", function(req, res) {
+    console.log("login page active");
+    res.render("login");
+});
 
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: process.env.RDS_HOSTNAME || "localhost",
-        user: process.env.RDS_USERNAME || "postgres",
-        password: process.env.RDS_PASSWORD || "password",
-        database: process.env.RDS_DB_NAME || "IntexFall",
-        port: process.env.RDS_PORT || 5432,
-        ssl:process.env.DB_SSL ? {rejectUnauthorized: false} : false
-    }
-})
-
-app.get("/", (req, res) => {
-    knex.select().from("survey").then( survey => {
-        res.render("displaySurvey", { mysurvey : survey });
-    })
-})
-
-app.listen(port, () => console.log("I am listening"))
+app.listen(app.get("port"), function (){
+    console.log("server started on port " + app.get("port"));
+});
