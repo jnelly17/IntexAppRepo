9dc663b895af87ac940868f0195cf1ca34848a0b
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-06 00:23:34 -0700

    v4

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d Intex23.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email jassinfosystems@gmail.com
+
+sudo certbot -n -d mental.health.provo.is404.net --nginx --agree-tos --email jassinfosystems@gmail.com
--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs"); // Corrected the setting for view engine
 
+app.use(express.urlencoded({ extended:true}))
+
+
 app.use("/", router); // Use the router for the root path
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -20,10 +23,7 @@ router.get("/survey", function(req, res) {
     console.log("survey page active");
     res.render("survey");
 });
-router.get("/awareness", function(req, res) {
-    console.log("awareness page active");
-    res.render("awareness");
-});
+
 router.get("/login", function(req, res) {
     console.log("login page active");
     res.render("login");
@@ -32,3 +32,23 @@ router.get("/login", function(req, res) {
 app.listen(app.get("port"), function (){
     console.log("server started on port " + app.get("port"));
 });
+
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.RDS_HOSTNAME || "localhost",
+        user: process.env.RDS_USERNAME || "postgres", //only ebroot has access to appdata
+        password: process.env.RDS_PASSWORD || "password",
+        database: process.env.RDS_DB_NAME || "IntexPractice",
+        port: process.env.RDS_PORT || 5432,
+        ssl:process.env.DB_SSL ? {rejectUnauthorized: false} : false
+    }
+})
+
+app.get("/awareness", (req, res) => {
+    knex.select().from("survey").then( survey => {
+
+        res.render("awareness", { mysurvey : survey });
+    })
+})
