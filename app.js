077ff332874100405dba036823db4705d3af77fa
jnelly17 <137342679+jnelly17@users.noreply.github.com>
2023-12-07 14:38:09 -0700

    finalFunctional

--- app.js
+++ app.js
@@ -29,70 +29,95 @@ const router = express.Router();
 // Set port and configure body-parser
 app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // PostgreSQL database connection pool
 const pool = new Pool({
     user: process.env.DB_USERNAME || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'IntexPractice',
+    database: process.env.DB_NAME || 'NormalizationTest',
     password: process.env.DB_PASSWORD || 'password',
-    port: process.env.DB_PORT || 5432,
+    port: process.env.RDS_PORT || 5432,
 });
 
-// Define route for form submission
-app.post('/submitForm', (req, res) => {
-    // Extract form data from the request
-    const { time, loc, q1, q2, q3, q4, q5, q6, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20 } = req.body;
+app.post("/submitForm", async (req, res) => {
+    try {
+        const surveyData = {
+            SurveyTime: new Date(),
+            City: req.body.location,
+            Age: req.body.age,
+            Gender: req.body.gender,
+            Relationship: req.body.relationStatus,
+            Occupation: req.body.occupStatus,
+            UseSocial: req.body.media,
+            Q8: req.body.time,
+            Q9: req.body.purpose,
+            Q10: req.body.distractFreq,
+            Q11: req.body.restless,
+            Q12: req.body.distract,
+            Q13: req.body.worries,
+            Q14: req.body.concentrate,
+            Q15: req.body.compare,
+            Q16: req.body.feel,
+            Q17: req.body.validation,
+            Q18: req.body.down,
+            Q19: req.body.interest,
+            Q20: req.body.sleep,
+        };
 
-    // First Query
-    const query = `
-        INSERT INTO Survey (SurveyTime, City, Age, Gender, Relationship, Occupation, UseSocial, Q8INT, Q9, Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17, Q18, Q19, Q20)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)
-    `;
+        // Insert survey data into the database
+        const insertedSurvey = await knex("Survey").insert(surveyData).returning("*");
 
-    const values = [time, loc, q1, q2, q3, q4, q6, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20];
+        // Log the inserted survey data to make sure that it is right
+        console.log("DB updated successfully:", insertedSurvey);
 
-    // Execute the first query
-        pool.query(query, values, async (error, results) => {
-        if (error) {
-            console.error('Error executing first query', error);
-            res.status(500).send('Internal Server Error');
-        } else {
-            // Second Query
-            const query2 = 'INSERT INTO Organization (OrgNum, Q5) VALUES ($1, $2)';
-            const pool2 = pool;
-            
-            await pool2.connect();
-            for (let i = 0; i < q5.length; i++) {
-                const orgNum = i + 1;
-                const org = q5[i];
-                const values2 = [orgNum, org];
-                await pool2.query(query2, values2);
+        // Now take the max survey number and assign it to the variable maxSurveyNumber
+        const maxSurveyNumber = await knex("Survey").max("SurveyNumber").first();
+        //check that shiz in the console
+        console.log("Max Survey Number:", maxSurveyNumber);
+
+        const Q5 = req.body.affiliations;
+            console.log(Q5);
+
+        try{
+            for (let i = 0; i < Q5.length; i++) {
+                const orgdata = {
+                    OrgNum: (i + 1),
+                    SurveyNumber: maxSurveyNumber['max'],
+                    Q5: Q5[i]
+                }
+                const inserteddata = await knex("Organization").insert(orgdata).returning("*");
+                console.log("Data successfully inserted into Organization table)",inserteddata)
             }
-
-            // Release pool2
-            pool2.release();
-
-            // Third Query
-            const query3 = 'INSERT INTO Platform (PlatNum, Q7) VALUES ($1, $2)';
-            const pool3 = pool;
-
-            await pool3.connect();
-            for (let i = 0; i < q7.length; i++) {
-                const PlatNum = i + 1;
-                const plat = q7[i];
-                const values3 = [PlatNum, plat];
-                await pool3.query(query3, values3);
-            }
-
-            // Release pool3
-            pool3.release();
-
-            console.log('Records inserted successfully.');
-            res.status(200).send('Survey submitted successfully!');
+        } catch(error) {
+            console.error("uh oh organization", error)
         }
-    });
+
+        const Q7 = req.body.platforms;
+            console.log(Q5);
+
+        try{
+            for (let i = 0; i < Q7.length; i++) {
+                const platdata = {
+                    PlatNum: (i + 1),
+                    SurveyNumber: maxSurveyNumber['max'],
+                    Q7: Q7[i]
+                }
+                const inserteddata = await knex("Platform").insert(platdata).returning("*");
+                console.log("Data successfully inserted into Platform table)", inserteddata)
+            }
+        } catch(error) {
+            console.error("uh oh biggie mistake in the platforms", error)
+        }
+
+        res.status(200).json({ success: true, message: "Form submitted successfully" });
+    } catch (error) {
+        console.error("Error submitting form:", error);
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
 });
+
+
 
 // Configure views and static files
 app.set("views", path.join(__dirname, "views"));
@@ -162,10 +187,14 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
     console.log(users);
 })
 
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('login')
-})
+app.get('/logout', function (req, res) {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 
 // Start the server
 app.listen(app.get("port"), () => {
@@ -179,23 +208,64 @@ const knex = require("knex")({
         host: process.env.RDS_HOSTNAME || "localhost",
         user: process.env.RDS_USERNAME || "postgres",
         password: process.env.RDS_PASSWORD || "password",
-        database: process.env.RDS_DB_NAME || "IntexPoop",
+        database: process.env.RDS_DB_NAME || "NormalizationTest",
         port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
     }
 });
 
-// Route to fetch survey data
-//app.get("/data", checkAuthenticated, (req, res) => {
-    //knex.select().from("survey").then(survey => {
-        //res.render("data", { mysurvey: survey });
-    //});
-//});
-
-router.get("/data", checkAuthenticated, (req, res) => {
-    console.log("data page active");
-    res.render("data");
+// Route to fetch survey and organization data
+app.get('/filter', checkAuthenticated, (req, res) => {
+    const selectedSurvey = req.query.surveyNum;
+    knex('Survey AS s')
+  .innerJoin(
+    knex.raw('(SELECT string_agg(org."Q5", \', \') as Organizations, org."SurveyNumber" FROM public."Organization" org group by org."SurveyNumber") AS sub1'),
+    'sub1.SurveyNumber',
+    's.SurveyNumber'
+  )
+  .innerJoin(
+    knex.raw('(SELECT string_agg(Plat."Q7", \', \') as Platforms, Plat."SurveyNumber" FROM public."Platform" Plat group by Plat."SurveyNumber") AS sub2'),
+    'sub2.SurveyNumber',
+    's.SurveyNumber'
+  )
+  .innerJoin(
+    knex.raw('(SELECT "Q8", "Q8INT" FROM public."Q8" Plat) AS sub3'),
+    'sub3.Q8INT',
+    's.Q8'
+  )
+  .select('*').from('Survey AS s')
+  .where('s.SurveyNumber', selectedSurvey)
+  .then(survey => {
+    console.log("I work", survey)
+    res.render("data", { mysurvey: survey });
 });
+console.log(selectedSurvey);''
+})
+
+app.get("/data", checkAuthenticated, (req, res) => {
+knex('Survey AS s')
+  .innerJoin(
+    knex.raw('(SELECT string_agg(org."Q5", \', \') as Organizations, org."SurveyNumber" FROM public."Organization" org group by org."SurveyNumber") AS sub1'),
+    'sub1.SurveyNumber',
+    's.SurveyNumber'
+  )
+  .innerJoin(
+    knex.raw('(SELECT string_agg(Plat."Q7", \', \') as Platforms, Plat."SurveyNumber" FROM public."Platform" Plat group by Plat."SurveyNumber") AS sub2'),
+    'sub2.SurveyNumber',
+    's.SurveyNumber'
+  )
+  .innerJoin(
+    knex.raw('(SELECT "Q8", "Q8INT" FROM public."Q8" Plat) AS sub3'),
+    'sub3.Q8INT',
+    's.Q8'
+  )
+  .select('*').from('Survey AS s').then(survey => {
+    console.log("I work", survey)
+    res.render("data", { mysurvey: survey });
+});
+});
+
+
 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()){
